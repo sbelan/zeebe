25a41d1776f8a477aec17adf38abc5cd102fe25d
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2018-06-19 09:43:27 +0200

    foo

---
+++ broker-core/src/test/java/io/zeebe/broker/workflow/BpmnPrototypeStreamProcessorTests.java
@@ -0,0 +1,37 @@
+package io.zeebe.broker.workflow;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import io.zeebe.broker.util.StreamProcessorRule;
+
+public class BpmnPrototypeStreamProcessorTests {
+
+
+  @Rule
+  public StreamProcessorRule streamProcessorRule = new StreamProcessorRule();
+
+  @Before
+  public void setUp()
+  {
+
+  }
+
+  @Test
+  public void shouldSplitOnParallelGateway()
+  {
+
+  }
+
+  @Test
+  public void shouldMergeOnParallelGateway()
+  {
+
+  }
+
+  @Test
+  public void shouldSynchronizeProcessCompletion()
+  {
+
+  }
+}
